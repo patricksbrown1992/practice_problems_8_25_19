21ca29983d81c37e70dc7b159411a81485fafedd
patrickbrown <patricksbrown1992@gmail.com>
2019-08-26 19:49:31 -0400

    missing_number_leetcode#268

--- missing_number_leetcode#268.rb
+++ missing_number_leetcode#268.rb
@@ -1,15 +1,5 @@
 def missing_number(nums)
-    length = nums.length
-    i = 0
-    hash = Hash.new(0)
-    while i <= length
-        hash[i] = 1
-        i += 1
-    end
-    nums.each do |num|
-        hash[num] -= 1
-    end
-    hash.sort_by {|k, v| -v}[0][0]
+    (0..nums.length).sum - nums.sum
 end
 
 p missing_number([3,0,1])
