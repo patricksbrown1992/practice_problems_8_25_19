c4e82c534740f991722d66eabbac2aa65f75616e
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-02-08 17:04:40 -0500

    solved 884

--- 884_leetcode_uncommon_words.js
+++ 884_leetcode_uncommon_words.js
@@ -1,4 +1,4 @@
-// needs work
+
 
 var uncommonFromSentences = function(A, B) {
     let ans = [];
@@ -6,14 +6,14 @@ var uncommonFromSentences = function(A, B) {
     B = B.split(' ');
     for(let i = 0; i < A.length; i++){
         let word1 = A[i];
-        if(!B.includes(word1) && A.join(' ').index(word1) == A.join(' ').lastIndex(word1)){
+        if(!B.includes(word1) && A.indexOf(word1) == A.lastIndexOf(word1)){
             ans.push(word1);
         }
     }
 
     for(let j = 0; j < B.length; j++){
         let word2 = B[j];
-        if(!A.includes(word2) && B.join(' ').index(word2) == B.join(' ').lastIndex(word2)){
+        if(!A.includes(word2) && B.indexOf(word2) == B.lastIndexOf(word2)){
             ans.push(word2);
         }
     }
