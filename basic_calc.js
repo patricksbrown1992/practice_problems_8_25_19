a3189e1799f8c2c37a1c9c6a086271112854109a
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-07-08 22:38:29 -0400

    solved basic calc in JS

---
+++ basic_calc.js
@@ -0,0 +1,17 @@
+function calculate(num1, operation, num2) {
+
+    if(operation == '+'){
+      return num1 + num2
+    } else if (operation == '-'){
+      return num1 - num2
+    }else if (operation == '/'){
+      if(num2 == 0){
+        return null;
+      }
+      return num1 / num2
+    } else if (operation == '*'){
+      return num1 * num2
+    } else {
+      return null;
+    }
+}
